80c614af5a339cacb497eab28689096e60bc9968
Devo Astroman <devo.astroman@gmail.com>
2025-03-13 16:40:22 +0100

    create selectTextbox

--- src/plugin-scripts/fruit-selector-app/sections/ReplacerSection.lua
+++ src/plugin-scripts/fruit-selector-app/sections/ReplacerSection.lua
@@ -3,6 +3,7 @@ local OnyxUI = require(script.Parent.Parent.Parent.packages.OnyxUI)
 local Toolbar = require(script.Parent.Parent.components.Toolbar)
 local ItemsBag = require(script.Parent.Parent.components.ItemsBag)
 local SelectBox = require(script.Parent.Parent.components.SelectBox)
+local SelectTextBox = require(script.Parent.Parent.components.SelectTextBox)
 
 
 return function(Scope: Fusion.Scope<any>, Props)
@@ -54,90 +55,84 @@ return function(Scope: Fusion.Scope<any>, Props)
         print("__ : ", peek(toolSelected))
         --[[ textLabel.Text = "Hello World! " .. peek(nTimesOpen) ]]
     end)
-
-    local itemsBag = ItemsBag(Scope, {
-        Items = { "line1", "line2", "line3" }
-     })
-
-     local tempFrame = Scope:Frame {
-		CornerRadius = UDim.new(0,0),
-        Size = UDim2.new(0, 400, 0, 20),
-        Padding = 0,
-        Content = {"button1"},
-        ContentWrapped = true,
-        ContentSize = 9,
-        OnActivated = function()
-            print("clicked 1")
-        end,
-	}
-
-     local btn1 = Scope:Button {
-        CornerRadius = UDim.new(0,0),
-        Size = UDim2.new(1, 0, 0, 30),
-        Padding = 0,
-        Content = {"button1"},
-        ContentWrapped = true,
-        ContentSize = 9,
-        OnActivated = function()
-            print("clicked 1")
-        end,
-    }
-
     
-
-    local btn2 = Scope:Button {
-        CornerRadius = UDim.new(0,0),
-        Size = UDim2.new(1, 0, 0, 20),
-        Padding = 0,
-        Content = {"button2"},
-        ContentWrapped = true,
-        ContentSize = 9,
-        OnActivated = function()
-            print("clicked 2")
-        end,
-    }
 
 
     local text1 = Scope:Text { 
-        Text = "Item1", 
-        Size = UDim2.new(.5, 0, 0, 20),
+        PaddingTop = UDim.new(0,5),
+        Text = "Item 1", 
+        Size = UDim2.new(1, 0, 0, 20),
         BackgroundColor3 = Util.Colors.Stone["50"],
         BackgroundTransparency = 0,
-        TextSize = 9,
-        TextXAlignment = Enum.TextXAlignment.Center,
+        TextSize = 9,        
         TextColor3 = Util.Colors.Stone["950"],
     }
 
-     local text2 = Scope:Text { Text = "Item2", Size = UDim2.new(1, 0, 0, 30),
-    BackgroundColor3 = Util.Colors.Green["200"],
-BackgroundTransparency = 0 }
-     local text3 = Scope:Text { Text = "Item3", Size = UDim2.new(1, 0, 0, 50),
-    BackgroundColor3 = Util.Colors.Blue["500"],
-BackgroundTransparency = 0 }
-     local text4 = Scope:Text { Text = "Item4", Size = UDim2.new(1, 0, 0, 40),
-    BackgroundColor3 = Util.Colors.Red["200"],
-BackgroundTransparency = 0 }
-     local text5 = Scope:Text { Text = "Item5", Size = UDim2.new(1, 0, 0, 35),
-    BackgroundColor3 = Util.Colors.Green["200"],
-BackgroundTransparency = 0 }
-     local text6 = Scope:Text { Text = "Item6", Size = UDim2.new(1, 0, 0, 50),
-    BackgroundColor3 = Util.Colors.Blue["200"],
-BackgroundTransparency = 0 }
+     local text2 = Scope:Text { 
+        PaddingTop = UDim.new(0,5),
+        Text = "Item 2", 
+        Size = UDim2.new(1, 0, 0, 20),
+        BackgroundColor3 = Util.Colors.Stone["50"],
+        BackgroundTransparency = 0,
+        TextSize = 9,        
+        TextColor3 = Util.Colors.Stone["950"],
+    }
 
-     local selectBox = SelectBox(Scope, {
-        Items = { text1, btn1, text3, text4, btn2, text6  }
-        --[[ Items = { btn1, text1, btn2  } ]]
+    local text3 = Scope:Text { 
+        PaddingTop = UDim.new(0,5),
+        Text = "Item 3", 
+        Size = UDim2.new(1, 0, 0, 20),
+        BackgroundColor3 = Util.Colors.Stone["50"],
+        BackgroundTransparency = 0,
+        TextSize = 9,        
+        TextColor3 = Util.Colors.Stone["950"],
+    }
+
+    local text4 = Scope:Text { 
+        PaddingTop = UDim.new(0,5),
+        Text = "Item 4", 
+        Size = UDim2.new(1, 0, 0, 20),
+        BackgroundColor3 = Util.Colors.Stone["50"],
+        BackgroundTransparency = 0,
+        TextSize = 9,        
+        TextColor3 = Util.Colors.Stone["950"],
+    }
+
+    local text5 = Scope:Text { 
+        PaddingTop = UDim.new(0,5),
+        Text = "Item 5", 
+        Size = UDim2.new(1, 0, 0, 20),
+        BackgroundColor3 = Util.Colors.Stone["50"],
+        BackgroundTransparency = 0,
+        TextSize = 9,        
+        TextColor3 = Util.Colors.Stone["950"],
+    }
+
+    local text6 = Scope:Text { 
+        PaddingTop = UDim.new(0,5),
+        Text = "Item 6", 
+        Size = UDim2.new(1, 0, 0, 20),
+        BackgroundColor3 = Util.Colors.Stone["50"],
+        BackgroundTransparency = 0,
+        TextSize = 9,        
+        TextColor3 = Util.Colors.Stone["950"],
+    }
+     local selectTextBox = SelectTextBox(Scope, {
+        Items = { text1, text2, text3, text4, text5  },        
+        Width = 100,
+        Height = 100
+
      })
 
 
     local parentFrame = Scope:Frame {
 		BackgroundTransparency = 1,
-        Size = UDim2.new(1, 0, 1, 0),
-        Position = UDim2.new(0, 0, 0, 0),
-        BackgroundColor3 = Util.Colors.Blue["700"]
+        Size = UDim2.new(0, 200, 1, 0),
+        Position = UDim2.new(0, 50, 0, 50),
+        BackgroundColor3 = Util.Colors.Gray["300"]
 	}
 
-    selectBox.Parent = parentFrame
+    selectTextBox.Parent = parentFrame
 
     return parentFrame
     
